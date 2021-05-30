96e3704c87f11e214487e717b7cc67f00474bd06
LAPTOP-DOIQIL6M\kesa <terenrey@gmail.com>
2021-05-30 17:17:20 +0700

    1. added methods for calculating integrals 2. cleared the "main" file

--- num_methods.cpp
+++ num_methods.cpp
@@ -3,41 +3,121 @@
 //
 #include <valarray>
 #include "num_methods.h"
-template <typename T>
-std::vector <db> autoGen(int n, db l, db r) {
+
+template<typename T>
+
+std::vector<db> autoGen(int n, db l, db r) {
     std::vector<db> x = std::vector<db>(n);
     for (int i = 0; i < n; i++)
         x[i] = l + i * (r - l) / (n - 1);
     return x;
 }
 
-/// <summary>
-/// Решение трехдиагональных СЛАУ методом прогонки
-/// </summary>
-/// <param name="a">коэффициенты элементов на диагонали, ниже главной</param>
-/// <param name="b">коэффициенты элементов на главной диагонали</param>
-/// <param name="c">коэффициенты элементов на диагонали, выше главной</param>
-/// <param name="d">столбец свободных членов</param>
-/// <returns>решение СЛАУ</returns>
+vector<db> AbstractIntegral::divideIntervalsInto2(const vector<db> &x) {
+    vector<db> x2 = vector < db > ();
+    x2.push_back(x[0]);
+    for (int i = 1; i < x.size(); i++) {
+        x2.push_back((x[i] + x[i - 1]) / 2);
+        x2.push_back(x[i]);
+    }
+    return x2;
+}
+
+vector<db> AbstractIntegral::calcPoints(db l, db r, db eps, db A, db B) {
+    //исходное разбиение отрезка на точки
+    vector < db > x = {l, r};
+    db h = x[1] - x[0];
+    //разделить каждый интервал на два,
+    //уменьшив шаг вдвое
+    vector < db > x2 = divideIntervalsInto2(x);
+    db intH = calcIntegral(x), intHalfH = calcIntegral(x2);
+    db diff = abs(intH - intHalfH) / (pow(2, degreeOnInterval - 1) - 1);
+    db mxmDiff = eps * h / (B - A);
+    if (diff > mxmDiff) {
+        vector < db > lPoints = calcPoints(l, (l + r) / 2., eps, A, B);
+        vector < db > rPoints = calcPoints((l + r) / 2., r, eps, A, B);
+        lPoints.insert(lPoints.end(), rPoints.begin() + 1, rPoints.end());
+        x = lPoints;
+    }
+    return x;
+}
+
+///составить вектор точек, расстояние между которыми задано пользователем
+vector<db> AbstractIntegral::getPointsWithFixedStep(db A, db B, int n) {
+    vector < db > x = vector < db > (n);
+    db h = (B - A) / db(n - 1);
+    for (int i = 0; i < n - 1; i++)
+        x[i] = A + h * i;
+    x[n - 1] = B;
+    return x;
+}
+
+
+db LeftRectangleIntegral::calcIntegral(vector<db> x) {
+    db res = 0;
+    for (int i = 1; i < x.size(); i++)
+        res += subFunc(x[i - 1]) * (x[i] - x[i - 1]);
+    return res;
+}
+
+db RightRectangleIntegral::calcIntegral(vector<db> x) {
+    db res = 0;
+    for (int i = 1; i < x.size(); i++)
+        res += subFunc(x[i]) * (x[i] - x[i - 1]);
+    return res;
+}
+
+db MiddleRectangleIntegral::calcIntegral(vector<db> x) {
+    db res = 0;
+    for (int i = 1; i < x.size(); i++) {
+        db h = x[i] - x[i - 1];
+        res += subFunc(x[i - 1] + h / 2.) * h;
+    }
+    return res;
+}
+
+db TrapeziumIntegral::calcIntegral(vector<db> x) {
+    db res = 0, yiPrev = subFunc(x[0]);
+    for (int i = 1; i < x.size(); i++) {
+        db h = x[i] - x[i - 1];
+        db yi = subFunc(x[i]);
+        res += (yi + yiPrev) / 2 * h;
+        yiPrev = yi;
+    }
+    return res;
+}
+
+db SimpsonsIntegral::calcIntegral(vector<db> x) {
+    //число точек должно быть четным
+    assert(x.size() % 2 == 1);
+    db res = 0, yPrev = subFunc(x[0]), yi = subFunc(x[1]);
+    for (int i = 1; i < x.size() - 1; i++) {
+        db h = x[i + 1] - x[i - 1];
+        db yNext = subFunc(x[i + 1]);
+        res += (yPrev + 4 * yi + yNext) * h;
+        yPrev = yi;
+        yi = yNext;
+    }
+    res /= 6.;
+    return res;
+}
 
 vector<db> ThomasAlgorithm(vector<db> a, vector<db> b, vector<db> c, vector<db> d) {
     int n = a.size();
-    vector<db> alpha = vector<db>(n + 1);
-    vector<db> beta = vector<db>(n + 1);
-    vector<db> x = vector<db>(n);
+    vector < db > alpha = vector < db > (n + 1);
+    vector < db > beta = vector < db > (n + 1);
+    vector < db > x = vector < db > (n);
     a[0] = c[c.size() - 1] = 0;
     //прямой ход прогонки: найти вспомогательные коэффициенты альфа и бета
     alpha[1] = c[0] / b[0];
     beta[1] = -1.0 * d[0] / b[0];
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
         alpha[i + 1] = c[i] / (b[i] - a[i] * alpha[i]);
         beta[i + 1] = (a[i] * beta[i] - d[i]) / (b[i] - a[i] * alpha[i]);
     }
     //обратный ход прогонки - найти искомые переменные
     x[n - 1] = beta[n];
-    for (int i = n - 2; i > 0; i--)
-    {
+    for (int i = n - 2; i > 0; i--) {
         x[i] = alpha[i + 1] * x[i + 1] + beta[i + 1];
     }
     x[0] = c[0] / b[0] * x[1] - d[1] / b[1];
@@ -55,26 +135,25 @@ vector<db> ThomasAlgorithm(vector<db> a, vector<db> b, vector<db> c, vector<db> 
 vector<vector<db>> Spline::getSplineMatrix(vector<db> x, vector<db> y) {
     assert(x.size() == y.size());
     int n = x.size();
-    vector<vector<db>> splMatr = vector<vector<db>>(4);
+    vector < vector < db >> splMatr = vector < vector < db >> (4);
     //в точках интерполяции коэффициент а равен
     //значению исходной функции
-    vector<db> a = y;
+    vector < db > a = y;
     // у векторов b и d нулевого элемента нет
     //вектора, соответствующие коэффициентам в сплайне
-    vector<db> b = vector<db>(n);
-    vector<db> c = vector<db>(n);
-    vector<db> d = vector<db>(n);
+    vector < db > b = vector < db > (n);
+    vector < db > c = vector < db > (n);
+    vector < db > d = vector < db > (n);
     //задаем трехдиагональную матрицу 4 векторами
     //вектора, соответствующие коэффициентам в трехдиагональной матрице (задающей
     //коэффициент 'с' сплайна)
-    vector<db> at = vector<db>(n, 0);
-    vector<db> bt = vector<db>(n, 0);
-    vector<db> ct = vector<db>(n, 0);
+    vector < db > at = vector < db > (n, 0);
+    vector < db > bt = vector < db > (n, 0);
+    vector < db > ct = vector < db > (n, 0);
     //вектор свободных членов
-    vector<db> dt = vector<db>(n, 0);
+    vector < db > dt = vector < db > (n, 0);
     //заполнить трехдиагональную матрицу и свободный член коэффициентов при c
-    for (int i = 1; i < n - 1; i++)
-    {
+    for (int i = 1; i < n - 1; i++) {
         db h1 = x[i] - x[i - 1];
         db h2 = x[i + 1] - x[i];
         at[i] = h1;
@@ -86,8 +165,7 @@ vector<vector<db>> Spline::getSplineMatrix(vector<db> x, vector<db> y) {
     bt[0] = bt[n - 1] = 1;
     c = ThomasAlgorithm(at, bt, ct, dt);
     //используя формулы, вычислить остальные коэффициенты
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
         db h1 = x[i] - x[i - 1];
         d[i] = (c[i] - c[i - 1]) / h1;
         b[i] = (h1 / 2) * c[i] - ((h1 * h1) / 6) * d[i] + (y[i] - y[i - 1]) / h1;
--- num_methods.h
+++ num_methods.h
@@ -2,9 +2,109 @@
 // Created by kesa on 29.05.2021.
 //
 #include "vect.h"
+
 #ifndef NUMERICAL_METHODS_NUM_METHODS_H
 #define NUMERICAL_METHODS_NUM_METHODS_H
 
+//Абстрактный класс интеграла, содержит основные
+//поля и методы
+class AbstractIntegral {
+private:
+    ///разделить каждый интервал на два
+    vector<db> divideIntervalsInto2(const vector<db> &x);
+
+    /// <summary>
+    ///автоматический выбор шага интегрирования
+    /// </summary>
+    /// <param name="l">текущая левая граница</param>
+    /// <param name="r"></param>
+    /// <param name="eps">точность</param>
+    /// <param name="A">левая граница интегрирования</param>
+    /// <param name="B"></param>
+    /// <returns></returns>
+    vector<db> calcPoints(db l, db r, db eps, db A, db B);
+
+protected:
+    //интегрируемая функция
+    db (*subFunc)(db x);
+
+//порядок точности метода
+    db degreeOnInterval;
+public:
+    AbstractIntegral(db(*func)(db x), int degree) : subFunc(func), degreeOnInterval(degree) {};
+
+/// вычислить значение интеграла, используя значения функции
+///в точках передаваемого вектора
+/// \param x
+/// \return
+    virtual db calcIntegral(vector<db> x) = 0;
+
+///  автоматический выбор шага интегрирования
+/// \param A левая граница
+/// \param B правая граница
+/// \param eps точность вычислений
+/// \return вектор точек интегрирования
+    vector<db> calcPoints(db A, db B, db eps) {
+        return calcPoints(A, B, eps, A, B);
+    }
+
+    ///составить вектор точек, расстояние между которыми задано пользователем
+    /// \param A левая граница
+    /// \param B правая граница
+    /// \param n количество точек
+    /// \return вектор точек интегрирования
+    vector<db> getPointsWithFixedStep(db A, db B, int n);
+};
+
+//интеграл, вычисляемый методом левых прямоугольников
+class LeftRectangleIntegral : public AbstractIntegral {
+public:
+    LeftRectangleIntegral(db(*FUNC)(db x)) : AbstractIntegral(FUNC, 2) {};
+
+    db calcIntegral(vector<db> x);
+};
+
+//интеграл, вычисляемый методом правых прямоугольников
+class RightRectangleIntegral : public AbstractIntegral {
+public:
+    RightRectangleIntegral(db(*FUNC)(db x)) : AbstractIntegral(FUNC, 2) {};
+
+    db calcIntegral(vector<db> x);
+};
+
+//интеграл, вычисляемый методом средних прямоугольников
+class MiddleRectangleIntegral : public AbstractIntegral {
+public:
+    MiddleRectangleIntegral(db(*FUNC)(db x)) : AbstractIntegral(FUNC, 3) {}
+
+    db calcIntegral(vector<db> x);
+};
+
+//интеграл, вычисляемый методом трапеций
+class TrapeziumIntegral : public AbstractIntegral {
+public:
+    TrapeziumIntegral(db(*FUNC)(db x)) : AbstractIntegral(FUNC, 3) {}
+
+    db calcIntegral(vector<db> x);
+
+};
+
+//интеграл, вычисляемый методом Симпсона
+class SimpsonsIntegral : public AbstractIntegral {
+public:
+
+    db calcIntegral(vector<db> x);
+
+    SimpsonsIntegral(db(*FUNC)(db x)) : AbstractIntegral(FUNC, 5) {}
+};
+
+/// Решение СЛАУ  с трехдиагональной матрицей
+///методом прогонки (методом Томаса)
+/// \param a коэффициенты элементов на диагонали, ниже главной
+/// \param b коэффициенты элементов на главной диагонали
+/// \param c коэффициенты элементов на диагонали, выше главной
+/// \param d столбец свободных членов
+/// \return решение СЛАУ
 vector<db> ThomasAlgorithm(vector<db> a, vector<db> b, vector<db> c, vector<db> d);
 
 struct Spline {
@@ -13,22 +113,26 @@ private:
     vector<vector<db>> SplineMatrix;
     vector<db> approxDot;
     vector<db> funcInDot;
+
     vector<vector<db>> getSplineMatrix(vector<db> x, vector<db> y);
+
 public:
-    Spline(vector<db> x, vector<db> y):approxDot(x),funcInDot(y),
-    SplineMatrix(getSplineMatrix(x, y)) {}
+    Spline(vector<db> x, vector<db> y) : approxDot(x), funcInDot(y),
+                                         SplineMatrix(getSplineMatrix(x, y)) {}
+
     //approximate func for one dot
     db getFuncApproxInDot(db curx);
+
     //approximate for vector of dots
-    vector<db> getFuncApproxInDots(vector<db> curx){
+    vector<db> getFuncApproxInDots(vector<db> curx) {
         vector<db> res = vector<db>(curx.size());
         for (int i = 0; i < curx.size(); ++i)
             res[i] = getFuncApproxInDot(curx[i]);
         return res;
     }
 };
-std::vector <db> autoGen(int n, db l, db r);
 
+std::vector<db> autoGen(int n, db l, db r);
 
 
 #endif //NUMERICAL_METHODS_NUM_METHODS_H
