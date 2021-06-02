6ac666a06fad2273e7d1b0163d7119b0d64d8966
LAPTOP-DOIQIL6M\kesa <terenrey@gmail.com>
2021-06-02 14:39:08 +0700

    a little refactoring

--- geometry.cpp
+++ geometry.cpp
@@ -4,12 +4,14 @@
 
 #include "geometry.h"
 
+
+
 bool section::intersect(section AB, section CD) {
     vect2d ab(AB.a, AB.b), ad(AB.a, CD.b), ac(AB.a, CD.a);
     if(vectProd(ab, ad) * vectProd(ab, ac) > 0)
         return false;
 
-    vect2d cd(CD.a, CD.b), ca(CD.a, AB.a), cb(CD.a, CD.b);
+    vect2d cd(CD.a, CD.b), ca(CD.a, AB.a), cb(CD.a, AB.b);
     if(vectProd(cd, ca) * vectProd(cd, cb) > 0)
         return false;
     return true;
@@ -20,6 +22,7 @@ section::section(point<db> A, point<db> B) :a(A), b(B){}
 db vectProd(vect2d a, vect2d b) {
     return a.x * b.y - a.y * b.x;
 }
+
 
 vect2d::vect2d(point<db> A, point<db> B) {
     x = B.x - A.x;
--- geometry.h
+++ geometry.h
@@ -4,21 +4,53 @@
 
 #ifndef NUMERICAL_METHODS_GEOMETRY_H
 #define NUMERICAL_METHODS_GEOMETRY_H
-#include "vect.h"
- struct vect2d {
-    db x, y;
-    vect2d (point<db> A, point<db> B);
 
+#include "vect.h"
+
+template<typename T>
+struct point {
+public:
+    T x = 0, y = 0;
+
+    point(T x, T y);
+
+    point() = default;
 };
+
+template<typename T>
+std::ostream &operator<<(ostream &fout, vector<point<T>> points);
+
+struct vect2d {
+    db x, y;
+    vect2d(point<db> A, point<db> B);
+};
+
 db vectProd(vect2d a, vect2d b);
 
-struct section{
+struct section {
     point<db> a, b;
+
     //check line segments AB and CD for intersection using vector product
     static bool intersect(section first, section second);
+
     section(point<db> A, point<db> B);
 
 };
 
+
+///DEFINITION OF TEMPLATE METHODS
+template<typename T>
+point<T>::point(T x, T y) : x(x), y(y) {}
+
+template<typename T>
+std::ostream &operator<<(ostream &fout, vector<point<T>> points) {
+    for (int i = 0; i < points.size(); i++)
+        fout << points[i].x << " ";
+    fout << endl;
+    for (int i = 0; i < points.size(); i++)
+        fout << points[i].y << " ";
+    fout << endl;
+    return fout;
+}
 
 #endif //NUMERICAL_METHODS_GEOMETRY_H
