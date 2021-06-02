f4582b367a86136fcee62d865d3b261f8c94dcaf
LAPTOP-DOIQIL6M\kesa <terenrey@gmail.com>
2021-06-02 14:41:29 +0700

    work with MonteCarlo method (10 lab completed)

--- integral.py
+++ integral.py
@@ -10,12 +10,14 @@ with open("output.txt") as fin:
         line = fin.readline()
         y.append(list(map(float, line.strip().split())))
 
-plt.plot(x[3], y[3],'.', label="in")
-plt.plot(x[4], y[4],'.', label="out")
+plt.plot(x[3], y[3],'.', label="in", markersize=2)
+plt.plot(x[4], y[4],'.', label="out", markersize=2)
 plt.plot(x[0], y[0],'.', label="Исходные точки")
 plt.plot(x[1], y[1], "-r", label="Ограничивающая область")
-plt.plot(x[2], y[2], label="Интерполирование")
+plt.plot(x[5], y[5],'.',label="Центр тяжести", markersize=4)
 
+plt.plot(x[2], y[2], label="Интерполяция")
+plt.xlabel("Площадь, вычисленная методом:\nСимпсона = " + str(x[6][0]) + "\nМонте-Карло = " + str(y[6][0]))
 
 plt.legend()
 
--- main.cpp
+++ main.cpp
@@ -7,16 +7,25 @@
 using namespace std;
 #define inf 1e12
 
+
 void printPoints (ostream& out, vector<point<db>> points) ;
 
 //figures dots
 
-vect<db> fx = vector<db>{2, 2, 2, 5, 8, 8, 8, 5, 2};
-vect<db> fy = vector<db>{7, 5, 3, 3, 3, 5, 7, 7, 7};
-vect<db> ft = vector<db>{1, 2, 3, 4, 5, 6, 7, 8, 9};
+vect<db> fx = vector<db>{0.278, 0.712, 0.898, 0.688, 0.244, 0.092, 0.302, 0.614, 0.624, 0.494, 0.336, 0.462, 0.332,
+0.22, 0.308, 0.52, 0.708, 0.722, 0.586, 0.336, 0.116, 0.056, 0.278};
+        //{0.336, 0.144, 0.15, 0.302, 0.522, 0.612, 0.644, 0.414, 0.336};
+        //{2, 2, 2, 5, 8, 8, 8, 5, 2};
+vect<db> fy = vector<db>{0.896, 0.868, 0.478, 0.116, 0.102, 0.478, 0.754, 0.662, 0.358, 0.272, 0.406, 0.522, 0.606,
+        0.388, 0.21, 0.176, 0.328, 0.646, 0.802, 0.8, 0.754, 0.878, 0.896};
+        //{0.82, 0.654, 0.456, 0.382, 0.352, 0.452, 0.626, 0.66, 0.82};
+        //{7, 5, 3, 3, 3, 5, 7, 7, 7};
+vect<db> ft = vector<db>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+//{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 int main() {
     db eps = 1e4;
+    int numbDots = 1000;
     ifstream fin("input.txt");
     ofstream fout("output.txt");
 
@@ -35,15 +44,21 @@ int main() {
 
 
 
-    cout <<  mc.calcArea(10000) << endl;
+   db mcArea = mc.calcDoubleIntegral(numbDots, [](db x, db y) { return 1; });
     //print area built by Spline and all random dots
     printPoints(fout, mc.areaDots);
     printPoints(fout, mc.dotsIn);
     printPoints(fout, mc.dotsOut);
 
 
+
+
+    db xDInt = mc.calcDoubleIntegral(numbDots, [](db x, db y) { return x; });
+    db yDInt = mc.calcDoubleIntegral(numbDots, [](db x, db y) { return y; });
+    fout  << xDInt/mcArea << endl << yDInt/mcArea << endl;
+
     //calculate area with SimpsonsMethod
-    cout << calcParamAreabySimpson(tGr, xGr, yGr);
+    fout << calcParamAreabySimpson(tGr, xGr, yGr) << endl << mcArea;
     fout.close();
     fin.close();
     system("python ..\\integral.py");
